df7023c0b2fbe8e77efb0be22414d68451f0e36c
Barma Vishwanth <vishwanthbsavp@gmail.com>
2024-07-15 16:09:53 +0530

    fix: project section

--- main.js
+++ main.js
@@ -30,6 +30,39 @@ document
     });
   });
 
+// Our Project Function
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".project-items .item");
+  const images = document.querySelectorAll(".project-image");
+
+  items[0].classList.add("active");
+  images[0].style.display = "block";
+  images[0].style.opacity = 1;
+
+  items.forEach((item) => {
+    item.addEventListener("click", function () {
+      items.forEach((i) => i.classList.remove("active"));
+      this.classList.add("active");
+      const index = this.getAttribute("data-index");
+
+      images.forEach((image) => {
+        image.style.opacity = 0;
+        setTimeout(() => {
+          image.style.display = "none";
+        }, 500);
+      });
+
+      const selectedImage = document.querySelector(
+        `.project-image[data-index="${index}"]`
+      );
+      setTimeout(() => {
+        selectedImage.style.display = "block";
+        selectedImage.style.opacity = 1;
+      }, 500);
+    });
+  });
+});
+
 // Slider Function
 let swiperCards = new Swiper(".card__content", {
   loop: true,
